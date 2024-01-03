d7b20e3841f98173bab4f879eb87e4e64298dc7d
gujaKhela <khelashviliguja@gmail.com>
2024-01-03 15:25:20 +0400

    testing some react router dom

--- src/components/LeftLine.jsx
+++ src/components/LeftLine.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import Icon from "../assets/Icon.png";
 import Todo from "../assets/TO-DO.png";
 import Tasks from "../assets/tasks.png";
@@ -8,7 +9,6 @@ import Completed from "../assets/completed.png";
 const LeftLine = () => {
   return (
     <div className="w-[380px] h-screen bg-[#DBEADD] ">
-        
       <div className="w-[300px] flex place-content-around py-8">
         <img src={Icon} alt="icon" />
         <img src={Todo} alt="Todo" />
@@ -23,20 +23,25 @@ const LeftLine = () => {
 
       <div className="w-full bg-[#C4DCD3]">
         <div className="flex items-center  m-5 py-2">
-          <img src={Importnat} alt="Important-icon" className="w-6 h-6 object-cover" />
+          <img
+            src={Importnat}
+            alt="Important-icon"
+            className="w-6 h-6 object-cover"
+          />
           <p className="px-5">important</p>
         </div>
       </div>
 
-      <div className="w-[380px] bg-[#C4DCD3] ">
-        <Link to="/completed" >
-        
+      <Link to="/completed" className="w-[380px] bg-[#C4DCD3] block">
         <div className="flex items-center m-5 py-2">
-          <img src={Completed} alt="Completed-icon" className="w-6 h-6 object-cover" />
+          <img
+            src={Completed}
+            alt="Completed-icon"
+            className="w-6 h-6 object-cover"
+          />
           <p className="px-5">completed</p>
         </div>
-        </Link>
-      </div>
+      </Link>
     </div>
   );
 };
