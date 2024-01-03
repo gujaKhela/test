61105036b9c4e96e754ab5709492eaad7200ae56
gujaKhela <khelashviliguja@gmail.com>
2024-01-03 15:29:23 +0400

    kkkkk

--- src/components/LeftLine.jsx
+++ src/components/LeftLine.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link } from 'react-router-dom';
 import Icon from "../assets/Icon.png";
 import Todo from "../assets/TO-DO.png";
 import Tasks from "../assets/tasks.png";
