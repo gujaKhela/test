65a77e3db76942530ba0044934215196b82651be
gujaKhela <khelashviliguja@gmail.com>
2024-01-07 11:47:18 +0400

    update handling completed page

--- src/pages/Completed.jsx
+++ src/pages/Completed.jsx
@@ -6,7 +6,7 @@ const AddTask = () => {
   const completedTasksString = localStorage.getItem("completedTasks");
   const completedTasks = JSON.parse(completedTasksString) || [];
 
-  console.log(completedTasks);
+  console.log("completedTasks on ", completedTasks);
 
   return (
     <>
@@ -14,36 +14,41 @@ const AddTask = () => {
         <LeftLine />
         <HeaderLine has_text={true} textContent="Completed Tasks" />
       </div>
-      {completedTasks.length?( <div className="absolute left-[420px] top-[26rem]">
-        <ul>
-        <p className="text-center text-xl font-semibold text-[#34574A]">
-          Completed Tasklist:
-        </p>
-          {completedTasks.map((completedTask) => (
-            <li
-              className="my-2 py-2 flex items-center line-through"
-              key={completedTask.task.id}
-            >
-              <span>
-                <div className="font-semibold inline-block line-through">Name</div> -{" "}
-                {completedTask.task.input}{" "}
-                <span className="ml-4">
-                  {" "}
-                  <div className="font-semibold inline-block line-through">Date</div> -{" "}
-                  {completedTask.task.date}
+      {completedTasks.length ? (
+        <div className="absolute left-[420px] top-[26rem]">
+          <ul>
+            <p className="text-center text-xl font-semibold text-[#34574A]">
+              Completed Tasklist:
+            </p>
+            {completedTasks.map((completedTask) => (
+              <li
+                className="my-2 py-2 flex items-center line-through"
+                key={completedTask.id}
+              >
+                <span>
+                  <div className="font-semibold inline-block line-through">
+                    Name
+                  </div>{" "}
+                  - {completedTask.input}{" "}
+                  <span className="ml-4">
+                    {" "}
+                    <div className="font-semibold inline-block line-through">
+                      Date
+                    </div>{" "}
+                    - {completedTask.date}
+                  </span>
                 </span>
-              </span>
-            </li>
-          ))}
-        </ul>
-      </div>) :(
-           <div className="absolute left-[420px] top-[26rem]">
-           <p className="text-center text-xl font-semibold">
-             Please complite some task first
-           </p>
-         </div>
+              </li>
+            ))}
+          </ul>
+        </div>
+      ) : (
+        <div className="absolute left-[420px] top-[26rem]">
+          <p className="text-center text-xl font-semibold">
+            Please complite some task first
+          </p>
+        </div>
       )}
-     
     </>
   );
 };
